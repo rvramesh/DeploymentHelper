1a07b81163ffebded0655f6e948724a271096c77
Ramesh Vijayaraghavan <contact@rvramesh.com>
2011-09-25 22:32:40 +0530

    Initial commit

---
+++ DeploymentHelper/ConfigurationSection.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Configuration;
+using System.IO;
+
+namespace DeploymentHelper
+{
+
+    public class ConfigSettingCollection
+    {
+        private List<MyConfigurationSection> _collection = new List<MyConfigurationSection>();
+
+        public void Add(MyConfigurationSection element)
+        {
+            _collection.Add(element);
+        }
+
+        public MyConfigurationSection Get(string appName, string environmentName)
+        {
+            var result = from item in _collection
+                         where item.EnvironmentName.Equals(environmentName,StringComparison.OrdinalIgnoreCase) &&
+                         item.AppName.Equals(appName, StringComparison.OrdinalIgnoreCase)
+                         select item;
+
+            if(result != null && result.Count()> 0)
+            {
+                return result.ElementAt(0);
+            }
+            return null;
+        }
+    }
+
+    public class ConfigurationLoader : ConfigurationSection
+    {
+
+        private ConfigurationLoader()
+        {
+        }
+
+        public static ConfigSettingCollection ScanAndGetConfig()
+        {
+            ConfigSettingCollection csc = new ConfigSettingCollection();
+            string[] files = Directory.GetFiles(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) , "*.config", SearchOption.TopDirectoryOnly);
+            
+            foreach (var file in files)
+            {
+
+                ConfigurationLoader c = new ConfigurationLoader(); 
+                c.Load(file);
+                csc.Add(c.Section);
+            }
+            return csc;
+        }
+
+        private void Load(string uri)
+        {
+            try
+            {
+                using (XmlReader reader = new XmlTextReader(uri))
+                {
+                    base.DeserializeElement(reader, false);
+                }
+            }
+            catch
+            {
+                //TODO: Handle cases where config files not belong to our config section.
+            }
+        }
+        
+        [ConfigurationProperty("appConfig")]
+        public MyConfigurationSection Section
+        {
+            get
+            {
+                return this["appConfig"] as MyConfigurationSection;
+            }
+            set
+            {
+                this["appConfig"] = value;
+            }
+        }
+    }
+
+    public class MyConfigurationSection : System.Configuration.ConfigurationElement
+    {
+
+
+        [ConfigurationProperty("environmentName")]
+        public string EnvironmentName
+        {
+            get
+            {
+                return this["environmentName"] as string;
+            }
+            set
+            {
+                this["environmentName"] = value;
+            }
+        }
+
+        [ConfigurationProperty("appName")]
+        public string AppName
+        {
+            get
+            {
+                return this["appName"] as string;
+            }
+            set
+            {
+                this["appName"] = value;
+            }
+        }
+
+
+        [ConfigurationProperty("appLocation")]
+        public string ConfigLocation
+        {
+            get
+            {
+                return this["appLocation"] as string;
+            }
+            set
+            {
+                this["appLocation"] = value;
+            }
+        }
+
+        [ConfigurationProperty("settings", Options = ConfigurationPropertyOptions.IsDefaultCollection)]
+        private MyConfigurationElementCollection SettingsInternal
+        {
+            get
+            {
+                return (MyConfigurationElementCollection)base["settings"];
+            }
+        }
+
+        public IEnumerable<MyConfigurationElement> Settings
+        {
+            get
+            {
+                for (int i = 0; i < SettingsInternal.Count; i++)
+                {
+                    yield return SettingsInternal[i];
+                }
+            }
+        }
+    }
+
+    public class MyConfigurationElement : ConfigurationElement
+    {
+        [ConfigurationProperty("key", DefaultValue = "")]
+        public string Key
+        {
+            get
+            {
+                return (string)base["key"];
+            }
+            set
+            {
+                base["key"] = value;
+            }
+        }
+
+        [ConfigurationProperty("attributeName", DefaultValue = "")]
+        public string AttributeName
+        {
+            get
+            {
+                return (string)base["attributeName"];
+            }
+            set
+            {
+                base["attributeName"] = value;
+            }
+        }
+
+        [ConfigurationProperty("matchExpression", DefaultValue = "")]
+        public string MatchExpression
+        {
+            get
+            {
+                return (string)base["matchExpression"];
+            }
+            set
+            {
+                base["matchExpression"] = value;
+            }
+        }
+
+        [ConfigurationProperty("replaceValue", DefaultValue = "")]
+        public string ReplaceValue
+        {
+            get
+            {
+                return (string)base["replaceValue"];
+            }
+            set
+            {
+                base["replaceValue"] = value;
+            }
+        }
+
+        [ConfigurationProperty("id", DefaultValue = "")]
+        public string Id
+        {
+            get
+            {
+                return (string)base["id"];
+            }
+            set
+            {
+                base["id"] = value;
+            }
+        }
+    }
+
+    [ConfigurationCollection(typeof(MyConfigurationElement))]
+    public class MyConfigurationElementCollection : ConfigurationElementCollection
+    {
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+
+            return new MyConfigurationElement();
+        }
+
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get
+            {
+                return ConfigurationElementCollectionType.BasicMap;
+            }
+        }
+
+        protected override string ElementName
+        {
+            get
+            {
+                return "setting";
+            }
+        }
+
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((MyConfigurationElement)element).Id;
+        }
+
+        public MyConfigurationElement this[int index]
+        {
+            get
+            {
+                return (MyConfigurationElement)base.BaseGet(index);
+            }
+            set
+            {
+                if (base.BaseGet(index) != null)
+                {
+                    base.BaseRemoveAt(index);
+                }
+                this.BaseAdd(index, value);
+            }
+        }
+    }
+}
---
+++ DeploymentHelper/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.XPath;
+using System.IO;
+
+namespace DeploymentHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var items = ConfigurationLoader.ScanAndGetConfig();
+
+            if (args.Length != 2)
+            {
+                WriteInfo("Arguments not specified. Usage DeploymentHelper.exe applicationName environmentName");
+                WriteInfo("e.g DeploymentHelper.exe deploymenttest QA");
+                return;
+            }
+
+            string appName = args[0];
+            string environmentName  = args[1];
+
+            var item = items.Get(appName, environmentName);
+
+            if (item == null)
+            {
+                WriteError(string.Format("Unable to find configuration file for {0} application's  {1} environment", appName, environmentName));
+                return;
+            }
+            else
+            {
+                WriteInfo(string.Format("Found configuration file for {0} application's  {1} environment", appName, environmentName));
+            }
+
+            if (item.Settings.Count() <= 0)
+            {
+                WriteError("No settings found in file.");
+                return;
+            }
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(item.ConfigLocation);
+            var navigator = doc.CreateNavigator();
+            foreach(var setting in item.Settings)
+            {
+                navigator.MoveToRoot();
+                XPathNodeIterator iterator = navigator.Select(setting.Key);
+
+                if (iterator.Count == 0)
+                    WriteWarn(string.Format("No matches found for key {0} when processing setting Id {1}", setting.Key, setting.Id));
+
+                while (iterator.MoveNext())
+                {
+                    if (iterator.Current.NodeType != XPathNodeType.Element)
+                    {
+                        throw new Exception(string.Format("XPath key for id {0}, is not pointing to an element", setting.Id));
+                    }
+                    XmlElement element = iterator.Current.UnderlyingObject as XmlElement;
+
+                    string j = element.GetAttribute(setting.AttributeName);
+
+                    string result = setting.ReplaceValue;
+                    if (!string.IsNullOrEmpty(setting.MatchExpression))
+                    {
+                        bool isMatching = Regex.Match(j, setting.MatchExpression,RegexOptions.IgnoreCase).Success;
+
+                        if (isMatching)
+                            result = Regex.Replace(j, setting.MatchExpression, setting.ReplaceValue, RegexOptions.IgnoreCase);
+                        else
+                            result = null;
+                    }
+                    else 
+                    {
+                        WriteWarn(string.Format("Match expression missing in id {0}", setting.Id));
+                    }
+
+                    if (result != null)
+                    {
+                        WriteReplacedInfo(j, result, element.OuterXml, setting.Id);
+                        element.RemoveAttribute(setting.AttributeName);
+                        element.SetAttribute(setting.AttributeName, result);
+                    }
+                }
+            }
+
+            string backupFileName=  item.ConfigLocation + "." + DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            
+            doc.Save(backupFileName );
+            WriteInfo(string.Format("Saving file {0} with updated configuration", backupFileName));
+
+            WriteWarn("Complete", false);
+        }
+
+        private static void WriteReplacedInfo(string contentToBeReplaced, string replaceWith, string actualXmlContent, string id)
+        {
+            WriteInfo("INFO >> Replacing ",false);
+            WriteWarn(contentToBeReplaced,false);
+            WriteInfo(" with ", false);
+            WriteWarn(replaceWith, false);
+            WriteInfo(string.Format(" in {0} for id {1}{2}",actualXmlContent, id,System.Environment.NewLine),false);
+
+        }
+
+        private static void WriteWarn(string p)
+        {
+            WriteWarn(p, true);
+        }
+
+
+        private static void WriteWarn(string p, bool displayWarn)
+        {
+            ConsoleColor original = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            if(displayWarn)
+                Console.WriteLine("WARN >> " + p);
+            else
+                Console.Write(p);
+            Console.ForegroundColor = original;
+        }
+
+        private static void WriteInfo(string p, bool displayInfo)
+        {
+             if(displayInfo)
+                Console.WriteLine("INFO >> " + p);
+            else
+                Console.Write(p);
+        }
+
+        private static void WriteInfo(string p)
+        {
+            WriteInfo(p, true);
+        }
+
+        private static void WriteError(string content)
+        {
+            ConsoleColor original = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR >> " + content);
+            Console.ForegroundColor = original;
+        }
+    }
+}
